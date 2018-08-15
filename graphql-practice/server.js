a6814c271cfc8a744e09b6141f47b3ba4985d3d4
Jufebrown <jufebrown@gmail.com>
2018-08-15 15:31:16 -0500

    comment out graphql.com code

--- graphql-practice/server.js
+++ graphql-practice/server.js
@@ -1,27 +1,39 @@
-var express = require("express");
-var graphqlHTTP = require("express-graphql");
-var { buildSchema } = require("graphql");
+/*********************
+Code from graphql.com 
+**********************/
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
+// var express = require("express");
+// var graphqlHTTP = require("express-graphql");
+// var { buildSchema } = require("graphql");
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return "Hello world!";
-  }
-};
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String
+//   }
+// `);
 
-var app = express();
-app.use("/graphql", graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-  })
-);
-app.listen(4000);
-console.log("Running a GraphQL API server at localhost:4000/graphql");
+// // The root provides a resolver function for each API endpoint
+// var root = {
+//   hello: () => {
+//     return "Hello world!";
+//   }
+// };
+
+// var app = express();
+// app.use("/graphql", graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true
+//   })
+// );
+// app.listen(4000);
+// console.log("Running a GraphQL API server at localhost:4000/graphql");
+
+
+
+
+/***************************
+Postgres and graphql tutorial
+***************************/
+
