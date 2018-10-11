3573bf6c7b03d313d7a63212d2c3d62429d87847
Jufebrown <jufebrown@gmail.com>
2018-10-11 17:00:00 -0500

    add richTextBox component

---
+++ vue-project/text-editor/src/components/RichTextBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="richTextBox">
+    <iframe name="textBox"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RichTextBox',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
