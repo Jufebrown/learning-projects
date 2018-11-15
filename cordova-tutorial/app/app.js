d07e048c7b9ca72bd774a1c74526ae03500db6eb
Jufebrown <jufebrown@gmail.com>
2018-11-15 09:48:57 -0600

    add button to capture image

--- cordova-tutorial/app/app.js
+++ cordova-tutorial/app/app.js
@@ -2,5 +2,9 @@ const App = angular.module('cordovaApp', []);
 
 App.controller('mainController', function($scope){
   $scope.message = 'Excited to build hybrid app';
+
+  $scope.captureImage = function() {
+    $scope.message = "Button clicked";
+  };
 });
 
